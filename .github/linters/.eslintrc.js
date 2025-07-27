d8eb09ee0458ce4c25a00cf1e4edd3cb3724f5de
Joseph Bryant <91710586+Joey-Bryant@users.noreply.github.com>
2025-07-26 22:08:41 -0400

    Create .eslintrc.js

---
+++ .github/linters/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': ['error', 4],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single']
+  }
+};
